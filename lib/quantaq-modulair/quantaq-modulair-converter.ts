30892b538c5ee57033a84a287b871c0dd16b6e44
David H. Hagan <david.hagan@quant-aq.com>
2023-01-23 22:30:54 -0500

    Completed unit tests with and without TSP

--- lib/quantaq-modulair/quantaq-modulair-converter.ts
+++ lib/quantaq-modulair/quantaq-modulair-converter.ts
@@ -1,20 +1,44 @@
 import { JtsDocument, TimeSeries } from '@eagle-io/timeseries'
 import { Converter } from '../../converter'
 import { parse } from 'csv-parse/sync'
+import { constants } from 'buffer';
+import { rawListeners } from 'process';
 
 /**
+ * Convert the output from QuantAQ's RESTful API for the MODULAIR
+ * product.
  * 
+ * Docs: https://docs.quant-aq.com/api
  */
 
 export class QuantAQModulairConverter extends Converter {
     convert (input: Buffer): JtsDocument {
-        const series1 = new TimeSeries({ name: 'first', type: 'NUMBER' });
+        const series1 = new TimeSeries({ name: 'sn', type: 'TEXT' })
+        const series2 = new TimeSeries({ name: 'pm1', type: 'NUMBER' })
+        const series3 = new TimeSeries({ name: 'pm25', type: 'NUMBER' })
+        const series4 = new TimeSeries({ name: 'pm10', type: 'NUMBER' })
+        const series5 = new TimeSeries({ name: 'tsp', type: 'NUMBER' })
+        const series6 = new TimeSeries({ name: 'rh', type: 'NUMBER' })
+        const series7 = new TimeSeries({ name: 'temp', type: 'NUMBER' })
 
+        // Get the records
+        const records = JSON.parse(input.toString())
 
-        records.forEach((row: {}) => {
+        // Iterate over each record and convert to eagle-io's format
+        records.data.forEach((row: {timestamp: string, sn: string, pm1: string, 
+                        pm25: string, pm10: string, tsp: string,  met: { rh: string, temp: string } }) => {
+
             const ts = new Date(row.timestamp)
+
+            series1.insert({ timestamp: ts, value: row.sn })
+            series2.insert({ timestamp: ts, value: Number(row.pm1) })
+            series3.insert({ timestamp: ts, value: Number(row.pm25) })
+            series4.insert({ timestamp: ts, value: Number(row.pm10) })
+            series5.insert({ timestamp: ts, value: Number(row.tsp) })
+            series6.insert({ timestamp: ts, value: Number(row.met.rh) })
+            series7.insert({ timestamp: ts, value: Number(row.met.temp) })
         })
 
-        return new JtsDocument({ series: [series1] })
+        return new JtsDocument({ series: [series1, series2, series3, series4, series5, series6, series7] })
     }
 }
--- lib/quantaq-modulair/test/quantaq-modulair-converter.test.ts
+++ lib/quantaq-modulair/test/quantaq-modulair-converter.test.ts
@@ -8,6 +8,56 @@ describe('Unit test for QuantAQ MODULAIR Sensors', function () {
         const buff = fs.readFileSync('lib/quantaq-modulair/test/input.dat')
         const result = converter.convert(buff)
 
-        expect(result.series[0].values).toEqual([])
+        // Check the serial number
+        expect(result.series[0].values).toEqual(['MOD-00264', 'MOD-00264', 'MOD-00264'])
+
+        // Check the PM1 data
+        expect(result.series[1].values).toEqual([0.81, 0.57, 0.66])
+
+        // Check the PM2.5 data
+        expect(result.series[2].values).toEqual([1.01, 0.69, 0.76])
+
+        // Check the PM10 data
+        expect(result.series[3].values).toEqual([1.64, 0.79, 0.86])
+
+        // Check the TSP data
+        expect(result.series[4].values).toEqual([1.64, 0.79, 0.86])
+
+        // Check the RH data
+        expect(result.series[5].values).toEqual([18.6, 18.5, 18.5])
+
+        // Check the Temp data
+        expect(result.series[6].values).toEqual([21.8, 21.8, 21.8])
+    })
+})
+
+// Run unit tests for a SKU=000 with no TSP model
+describe('Unit test for QuantAQ MODULAIR Sensor without TSP enabled', function () {
+    it('converts sample file', async() => {
+        const converter = new QuantAQModulairConverter()
+
+        const buff = fs.readFileSync('lib/quantaq-modulair/test/input-2.dat')
+        const result = converter.convert(buff)
+
+        // Check the serial number
+        expect(result.series[0].values).toEqual(['MOD-00264', 'MOD-00264', 'MOD-00264'])
+
+        // Check the PM1 data
+        expect(result.series[1].values).toEqual([0.81, 0.57, 0.66])
+
+        // Check the PM2.5 data
+        expect(result.series[2].values).toEqual([1.01, 0.69, 0.76])
+
+        // Check the PM10 data
+        expect(result.series[3].values).toEqual([1.64, 0.79, 0.86])
+
+        // Check the TSP data
+        expect(result.series[4].values).toEqual([NaN, NaN, NaN])
+
+        // Check the RH data
+        expect(result.series[5].values).toEqual([18.6, 18.5, 18.5])
+
+        // Check the Temp data
+        expect(result.series[6].values).toEqual([21.8, 21.8, 21.8])
     })
 })
