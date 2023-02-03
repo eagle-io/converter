a0812b8773ef56aa19e7108804ceb8334e768158
ramod8 <amod.ridwaan@gmail.com>
2023-02-03 15:22:04 +1000

    remove duplicate headers in input file

--- lib/aecom-noise/aecom-noise-converter.ts
+++ lib/aecom-noise/aecom-noise-converter.ts
@@ -17,12 +17,14 @@ export class AecomNoiseConverter extends Converter {
     const firstLineIndex = data.indexOf('\n')
     const csv = data.substring(firstLineIndex + 1) // Remove first line to process leftover string as csv
     const firstLine = data.substring(0, firstLineIndex)
+
     const records = parse(csv, { delimiter: ';', relax_column_count: true, columns: true })
     const logDate = firstLine.split(';')[8] // Date is always stored in this location
 
-    // Generate list of all data series
-    const headers = Object.keys(records[0]).filter(m => {
-      return m !== 'Time'
+    // Generate list of all data series. Removes duplicate headers
+    const fileHeader = Object.values(csv.split('\n', 1)[0].split(';'))
+    const headers = fileHeader.filter(m => {
+      return (m !== 'Time' && fileHeader.indexOf(m) === fileHeader.lastIndexOf(m))
     })
 
     // Create JTS series for each data series
@@ -36,10 +38,14 @@ export class AecomNoiseConverter extends Converter {
         break
       }
 
-      const ts = new Date(`${logDate} ${row.Time}`)
-
       for (const header of headers) {
-        allSeries[header].insert({ timestamp: ts, value: Number(row[header]) })
+        const ts = new Date(`${logDate} ${row.Time}`)
+
+        if (header === 'Latitude' || header === 'Longitude') {
+          allSeries[header].insert({ timestamp: ts, value: row[header] })
+        } else {
+          allSeries[header].insert({ timestamp: ts, value: Number(row[header]) })
+        }
       }
     }
 
