c81c7d145959945c098e61fb3a82007570a53e43
ramod8 <amod.ridwaan@gmail.com>
2023-02-03 12:28:39 +1000

    make aecom noise converter generic

--- lib/aecom-noise/aecom-noise-converter.ts
+++ lib/aecom-noise/aecom-noise-converter.ts
@@ -7,25 +7,42 @@ import { parse } from 'csv-parse/sync'
  */
 export class AecomNoiseConverter extends Converter {
   convert (input: Buffer): JtsDocument {
-    const series1 = new TimeSeries({ name: 'LAeq', type: 'NUMBER' })
+    interface SeriesObject {
+      [key: string]: any
+  }
 
-    const csv = input.toString('utf-8')
-    const records = parse(csv, { delimiter: ';', relax_column_count: true })
-    const logDate = records[0][8] // Date is always stored in this location
+    const allSeries: SeriesObject = {}
 
-    for (const [index, row] of records.entries()) {
+    const data = input.toString('utf-8')
+    const firstLineIndex = data.indexOf('\n')
+    const csv = data.substring(firstLineIndex + 1) // Remove first line to process leftover string as csv
+    const firstLine = data.substring(0, firstLineIndex)
+    const records = parse(csv, { delimiter: ';', relax_column_count: true, columns: true })
+    const logDate = firstLine.split(';')[8] // Date is always stored in this location
+
+    // Generate list of all data series
+    const headers = Object.keys(records[0]).filter(m => {
+      return m !== 'Time'
+    })
+
+    // Create JTS series for each data series
+    for (const header of headers) {
+      allSeries[header] = new TimeSeries({ name: header, type: 'NUMBER' })
+    }
+
+    for (const row of records) {
       // Stop processing file after the first blank row
-      if (row[0] === '') {
+      if (row.Time === '') {
         break
       }
 
-      // Sensor data is located from row 3 onwards
-      if (index > 1) {
-        const ts = new Date(`${logDate} ${row[0]}`)
-        series1.insert({ timestamp: ts, value: Number(row[1]) })
+      const ts = new Date(`${logDate} ${row.Time}`)
+
+      for (const header of headers) {
+        allSeries[header].insert({ timestamp: ts, value: Number(row[header]) })
       }
     }
 
-    return new JtsDocument({ series: [series1] })
+    return new JtsDocument({ series: Object.values(allSeries) })
   }
 }
