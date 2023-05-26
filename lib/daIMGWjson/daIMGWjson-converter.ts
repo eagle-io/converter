b65b31f64b99eb7b13f822edc2ec09ee811e2743
beol74 <106877344+beol74@users.noreply.github.com>
2023-05-26 08:56:14 +0200

    add parameters (Airtemp, velocity)

--- lib/daIMGWjson/daIMGWjson-converter.ts
+++ lib/daIMGWjson/daIMGWjson-converter.ts
@@ -2,25 +2,42 @@ import { JtsDocument, TimeSeries } from '@eagle-io/timeseries'
 import { Converter } from '../../converter'
 
 /**
- * Convert the JSON data from IMGW 
+ * Convert the JSON data from IMGW meteo station
  * exemple : https://hydro.imgw.pl/api/station/meteo/?id=250200370
  * 
  *
  */
 export class daimgwjsonConverter extends Converter {
   convert (input: Buffer): JtsDocument {
-    const series1 = new TimeSeries({ name: 'hourlyPrecip'})    
+    const series1 = new TimeSeries({ name: 'hourlyPrecip'})  
+    const series2 = new TimeSeries({ name: 'airTemperatureRecords'})  
+    const series3 = new TimeSeries({ name: 'windVelocityObsRecords'})  
+
     // Get the records
     const records = JSON.parse(input.toString())
 
-    // Iterate over each record and convert to eagle-io's format
+    // Iterate over each record hourlyPrecipRecords and convert to eagle-io's format
     records.hourlyPrecipRecords.forEach((row: {
       date: string,
       value: string }) => {
       const ts = new Date(row.date)
       series1.insert({ timestamp: ts, value: Number(row.value) })
     })
-
+    // Iterate over each record airTemperatureRecords and convert to eagle-io's format
+    records.airTemperatureRecords.forEach((row: {
+      date: string,
+      value: string }) => {
+      const ts = new Date(row.date)
+      series2.insert({ timestamp: ts, value: Number(row.value) })
+    })
+    // Iterate over each record windVelocityObsRecords and convert to eagle-io's format
+    records.windVelocityObsRecords.forEach((row: {
+      date: string,
+      value: string }) => {
+      const ts = new Date(row.date)
+      series1.insert({ timestamp: ts, value: Number(row.value) })	
+	})
+	  
     return new JtsDocument({ series: [series1] })
   }
 }
