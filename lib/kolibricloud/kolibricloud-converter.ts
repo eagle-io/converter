747d14d2be7482376c483ef129e676117ff05920
Jarrah Watson <jarrah@eagle.io>
2023-08-15 13:09:39 +1000

    add kolibricloud converter

--- lib/kolibricloud/kolibricloud-converter.ts
+++ lib/kolibricloud/kolibricloud-converter.ts
@@ -1,44 +1,39 @@
 import { JtsDocument, TimeSeries } from '@eagle-io/timeseries'
 import { Converter } from '../../converter'
-import * as fs from 'fs'
+import { Units, MeasurementDefinitions } from './kolibri-param-unit-id-list'
 
 /**
  * Convert the JSON data from kolibricloud.ch (Keller water level sensors)
  * Documentation https://docs.kolibricloud.ch/cloud-interfaces/api/access_details/
- * Exemple: https://api.kolibricloud.ch/v1/Measurements?measurementDefinitionId=11&deviceId=2405&start=2023-08-07&end=2023-08-08&isFiltered=false
- *
+ * Example: https://api.kolibricloud.ch/v1/Measurements?measurementDefinitionId=11&deviceId=2405&start=2023-08-07&end=2023-08-08&isFiltered=false
  */
 export class KolibriCloudConverter extends Converter {
   convert (input: Buffer): JtsDocument {
     // Get the records
     const records = JSON.parse(input.toString())
 
-    // Get the parameters and units list
-    const buff = fs.readFileSync('lib/kolibricloud/kolibri_param_unit_id_list.txt')
-    const paramunit = JSON.parse(buff.toString())
-
-    const findUnit = (id:string) => {
-      return paramunit.units.find((unit:any) => unit.id === id).unitSymbol
+    const findUnit = (id:number) => {
+      return Units.find((unit) => unit.id === id)?.unitSymbol
     }
-    const findParam = (id:string) => {
-      return paramunit.measurementsDefinitionIds.find((param:any) => param.id === id).name
+    const findParam = (id:number) => {
+      return MeasurementDefinitions.find((param) => param.id === id)?.name
     }
 
     // Get the parameter and unit name
-    const param = findParam(records.measurementDefinitionId)
-    const unit = findUnit(records.unitId)
+    const param = findParam(records.measurementDefinitionId) || records.measurementDefinitionId
+    const unit = findUnit(records.unitId) || ''
 
     // Create JTS series
-    const serie = new TimeSeries({ name: param, units: unit, type: 'NUMBER' })
+    const series = new TimeSeries({ name: param, units: unit, type: 'NUMBER' })
 
     // Iterate over each record and convert to eagle-io's format
     records.values.forEach((row: {
       time: string,
       value: string }) => {
       const ts = new Date(row.time + 'Z')
-      serie.insert({ timestamp: ts, value: Number(row.value) })
+      series.insert({ timestamp: ts, value: Number(row.value) })
     })
 
-    return new JtsDocument({ series: [serie] })
+    return new JtsDocument({ series: [series] })
   }
 }
