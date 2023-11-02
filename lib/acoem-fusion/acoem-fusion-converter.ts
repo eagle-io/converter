08cc3f0a54b47bf7d122b5e3da1d7eeb9bd405cb
Josh W <41724625+Joshwantt@users.noreply.github.com>
2023-11-02 10:24:38 +1000

    refactor and add timezones

--- lib/acoem-fusion/acoem-fusion-converter.ts
+++ lib/acoem-fusion/acoem-fusion-converter.ts
@@ -1,41 +1,74 @@
 import { JtsDocument, TimeSeries } from '@eagle-io/timeseries'
 import { Converter } from '../../converter'
 
-
 /**
- * Converts sample AcoemXML -> JTS.
- */
+* Converts sample AcoemXML -> JTS.
+*/
 export class AcoemFusionConverter extends Converter {
-  convert(input: Buffer): JtsDocument {
+  convert(input: Buffer, timezone: string): JtsDocument {
     const xml = input.toString('utf-8')
 
-    let ts: Date = new Date(Date.now());
+    const series: {
+      [key: string]: any
+    } = {}
 
+    //regex out xml values
     const timestamp = /(\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2})/.exec(xml);
-
-    if (timestamp != null) {
-      ts = new Date(((timestamp[1]+"Z").replace("/","-").replace("/","-").replace(" ","T")))
-    }
-
-    const WindSpeedSeries = new TimeSeries({ name: 'WindSpeed', type: 'NUMBER' })
-    const WindDirectionSeries = new TimeSeries({ name: 'WindDirection', type: 'NUMBER' })
-    const RainIntensitySeries = new TimeSeries({ name: 'RainIntensity', type: 'NUMBER' })
-    const BarometricPressureSeries = new TimeSeries({ name: 'BarometricPressure', type: 'NUMBER' })
-    const AirTemperatureSeries = new TimeSeries({ name: 'AirTemperature', type: 'NUMBER' })
-    const RelativeHumiditySeries = new TimeSeries({ name: 'RelativeHumidity', type: 'NUMBER' })
-
+    const values  = /<Values>(.*?)<\/Values>/.exec(xml)
+    const spec1  = /<Spectrum1>(.*?)<\/Spectrum1>/.exec(xml)
+    const spec2  = /<Spectrum2>(.*?)<\/Spectrum2>/.exec(xml)
     const weather  = /<Weather>(.*?)<\/Weather>/.exec(xml)
 
-    if (weather != null) {
-      const weatherArray = weather[1].split(';')
-      WindSpeedSeries.insert({ timestamp: ts, value: Number(weatherArray[0]) })
-      WindDirectionSeries.insert({ timestamp: ts, value: Number(weatherArray[1]) })
-      RainIntensitySeries.insert({ timestamp: ts, value: Number(weatherArray[2]) })
-      BarometricPressureSeries.insert({ timestamp: ts, value: Number(weatherArray[3]) })
-      AirTemperatureSeries.insert({ timestamp: ts, value: Number(weatherArray[4]) })
-      RelativeHumiditySeries.insert({ timestamp: ts, value: Number(weatherArray[5]) })
+    if (timestamp != null) {
+      //format time and create new date
+
+      const ts = this.dayjs.tz(((timestamp[1]).replace("/","-").replace("/","-").replace(" ","T")), timezone)
+
+      if (values != null) {
+        const valuesArray = values[1].split(';')
+        for (let i = 0; i < valuesArray.length; i++) {
+          const name: string = 'value_'+String(i+1)
+          // Create JTS series for each data series
+          series[name] = new TimeSeries({ name: name, type: 'NUMBER' })
+          // Insert the measures
+          series[name].insert({ timestamp: ts.toDate(), value: Number(valuesArray[i]) })
+        }
+      }
+
+      if (spec1 != null) {
+        const specArray = spec1[1].split(';')
+        for (let i = 0; i < specArray.length; i++) {
+          const name: string = 'spec1_'+String(i+1)
+          // Create JTS series for each data series
+          series[name] = new TimeSeries({ name: name, type: 'NUMBER' })
+          // Insert the measures
+          series[name].insert({ timestamp: ts.toDate(), value: Number(specArray[i]) })
+        }
+      }
+
+      if (spec2 != null) {
+        const specArray = spec2[1].split(';')
+        for (let i = 0; i < specArray.length; i++) {
+          const name: string = 'spec2_'+String(i+1)
+          // Create JTS series for each data series
+          series[name] = new TimeSeries({ name: name, type: 'NUMBER' })
+          // Insert the measures
+          series[name].insert({ timestamp: ts.toDate(), value: Number(specArray[i]) })
+        }
+      }
+
+      if (weather != null) {
+        const weatherArray = weather[1].split(';')
+        for (let i = 0; i < weatherArray.length; i++) {
+          const name: string = 'weather_'+String(i+1)
+          // Create JTS series for each data series
+          series[name] = new TimeSeries({ name: name, type: 'NUMBER' })
+          // Insert the measures
+          series[name].insert({ timestamp: ts.toDate(), value: Number(weatherArray[i]) })
+        }
+      }
     }
-    
-    return new JtsDocument({ series: [WindSpeedSeries, WindDirectionSeries, RainIntensitySeries, BarometricPressureSeries, AirTemperatureSeries, RelativeHumiditySeries] });
+
+    return new JtsDocument({ series: Object.values(series) });
   }
 }
