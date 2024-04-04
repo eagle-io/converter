998232bbcdf4cbd3d90a647ef38d90c7a62a3b71
Jared Briskman <github@jaredbriskman.com>
2024-04-04 18:25:55 -0400

    Add support for MODULAIR gases, and fix QA/QC parsing bug

--- lib/quantaq-modulair/test/quantaq-modulair-converter.test.ts
+++ lib/quantaq-modulair/test/quantaq-modulair-converter.test.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs'
 import { QuantAQModulairConverter } from '../quantaq-modulair-converter'
 
-describe('Unit test for QuantAQ MODULAIR Sensors', function () {
+describe('Unit test for SKU=000 QuantAQ MODULAIR Sensors', function () {
   it('converts sample file', async () => {
     const converter = new QuantAQModulairConverter()
     const buff = fs.readFileSync('lib/quantaq-modulair/test/input.dat')
@@ -33,11 +33,26 @@ describe('Unit test for QuantAQ MODULAIR Sensors', function () {
 
     // Check the WD data
     expect(result.series[8].values).toEqual([0, 0, 0])
+
+    // Check the NO data
+    expect(result.series[9].values).toEqual([NaN, NaN, NaN])
+
+    // Check the NO2 data
+    expect(result.series[10].values).toEqual([NaN, NaN, NaN])
+
+    // Check the O3 data
+    expect(result.series[11].values).toEqual([NaN, NaN, NaN])
+
+    // Check the CO data
+    expect(result.series[12].values).toEqual([NaN, NaN, NaN])
+
+    // Check the CO2 data
+    expect(result.series[13].values).toEqual([NaN, NaN, NaN])
   })
 })
 
 // Run unit tests for a SKU=000 with no TSP model
-describe('Unit test for QuantAQ MODULAIR Sensor without TSP enabled', function () {
+describe('Unit test for SKU=000 QuantAQ MODULAIR Sensor without TSP enabled', function () {
   it('converts sample file', async () => {
     const converter = new QuantAQModulairConverter()
     const buff = fs.readFileSync('lib/quantaq-modulair/test/input-2.dat')
@@ -69,5 +84,71 @@ describe('Unit test for QuantAQ MODULAIR Sensor without TSP enabled', function (
 
     // Check the WD data
     expect(result.series[8].values).toEqual([NaN, NaN, NaN])
+
+    // Check the NO data
+    expect(result.series[9].values).toEqual([NaN, NaN, NaN])
+
+    // Check the NO2 data
+    expect(result.series[10].values).toEqual([NaN, NaN, NaN])
+
+    // Check the O3 data
+    expect(result.series[11].values).toEqual([NaN, NaN, NaN])
+
+    // Check the CO data
+    expect(result.series[12].values).toEqual([NaN, NaN, NaN])
+
+    // Check the CO2 data
+    expect(result.series[13].values).toEqual([NaN, NaN, NaN])
+  })
+})
+
+// Run unit tests for a SKU=015 with no TSP model
+describe('Unit test for SKU=015 QuantAQ MODULAIR Sensor without TSP enabled', function () {
+  it('converts sample file', async () => {
+    const converter = new QuantAQModulairConverter()
+    const buff = fs.readFileSync('lib/quantaq-modulair/test/input-3.dat')
+    const result = converter.convert(buff)
+
+    // Check the serial number
+    expect(result.series[0].values).toEqual(['MOD-RND-2', 'MOD-RND-2', 'MOD-RND-2'])
+
+    // Check the PM1 data
+    expect(result.series[1].values).toEqual([0.03, NaN, 0.08])
+
+    // Check the PM2.5 data
+    expect(result.series[2].values).toEqual([0.2, NaN, 0.18])
+
+    // Check the PM10 data
+    expect(result.series[3].values).toEqual([1.69, NaN, 0.34])
+
+    // Check the TSP data
+    expect(result.series[4].values).toEqual([NaN, NaN, NaN])
+
+    // Check the RH data
+    expect(result.series[5].values).toEqual([37.5, 37.6, 37.5])
+
+    // Check the Temp data
+    expect(result.series[6].values).toEqual([16.2, 16.2, 16.2])
+
+    // Check the WS data
+    expect(result.series[7].values).toEqual([0, 0, 0])
+
+    // Check the WD data
+    expect(result.series[8].values).toEqual([0, 0, 0])
+
+    // Check the NO data
+    expect(result.series[9].values).toEqual([6.82, 6.75, 6.16])
+
+    // Check the NO2 data
+    expect(result.series[10].values).toEqual([9.77, 9.54, 9.77])
+
+    // Check the O3 data
+    expect(result.series[11].values).toEqual([14.8, 15.65, 15.39])
+
+    // Check the CO data
+    expect(result.series[12].values).toEqual([487.53, 487.05, 473.16])
+
+    // Check the CO2 data
+    expect(result.series[13].values).toEqual([NaN, NaN, NaN])
   })
 })
