137bc3aae767e41295b83f77df66f84528c1f09a
Ethan-beca <ethan.fang@beca.com>
2024-11-15 14:24:16 +1300

    Update adroitvw-converter.ts

--- lib/AdroitVW/adroitvw-converter.ts
+++ lib/AdroitVW/adroitvw-converter.ts
@@ -1,27 +1,27 @@
 import { JtsDocument, TimeSeries } from '@eagle-io/timeseries';
 import { Converter } from '../../converter';
-
+import moment from 'moment-timezone';
+ 
 /**
- * Convert the JSON data from Adroit VW Logger
- * example: https://adroitplatform.com/api/points/34404/feeds/54553
- */
+* Convert the JSON data from Adroit VW Logger
+* example: https://adroitplatform.com/api/points/34404/feeds/54553
+*/
 export class AdroitVWJsonConverter extends Converter {
   convert(input: Buffer): JtsDocument {
-    const series1 = new TimeSeries({ name: 'frequency' });
-
-    // Parse the input buffer to JSON object
+  // Parse the input buffer to JSON object
     const record = JSON.parse(input.toString());
+    // Convert the epoch time to a moment object
+	let timestamp = moment.utc(record.lastUpdate).tz('Pacific/Auckland').toDate();
+	
+	 // Create a TimeSeries object
+	const series1 = new TimeSeries({ name: record.name });
 
-    // Convert the single frequency value to eagle-io's format
-    //const timestamp = new Date(record.lastUpdate);  // using 'lastUpdate' as timestamp
-	//const timestamp = new Date(record.lastUpdate * 1000).toLocaleString("en-NZ", { timeZone: "Pacific/Auckland" });
-	const timestamp_ep = new Date(record.lastUpdate);
-    const timestamp = new Date(timestamp_ep.getTime() + (12 * 60 * 60 * 1000)); 
-    const value = Number(record.value);  // converting value to number
-
+	// Convert the value to a number
+    const value = Number(record.value);  
+ 
     // Insert the time series data
     series1.insert({ timestamp, value });
-
+ 
     // Return the JTS document
     return new JtsDocument({ series: [series1] });
   }
