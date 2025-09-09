1e402c402f35d403bbd3a54205fd9db6b7a017e5
Nicholas Hitchins <nick.hitchins@gmail.com>
2025-09-10 08:05:56 +1000

    Fix unit tests

--- lib/darubixjson/test/darubixjson-converter.test.ts
+++ lib/darubixjson/test/darubixjson-converter.test.ts
@@ -7,7 +7,7 @@ describe('Unit test for darubixjson', function () {
     const buff = fs.readFileSync('lib/darubixjson/test/input.dat')
     const result = converter.convert(buff)
 
-    expect(result.series[0].first.timestamp).toEqual(new Date('2025-03-20T07:30:21.101Z'))
+    expect(result.series[0].first?.timestamp).toEqual(new Date('2025-03-20T07:30:21.101Z'))
 
     expect(result.series[0].values).toEqual([-150.0])
     expect(result.series[0].name).toEqual('waterlevel')
@@ -15,10 +15,10 @@ describe('Unit test for darubixjson', function () {
 
     expect(result.series[1].values).toEqual([0.0])
     expect(result.series[1].name).toEqual('temperature')
-    expect(result.series[1].first.timestamp).toEqual(new Date('2025-03-20T07:30:21.101Z'))
+    expect(result.series[1].first?.timestamp).toEqual(new Date('2025-03-20T07:30:21.101Z'))
 
     expect(result.series[2].values).toEqual([5000.0])
     expect(result.series[2].name).toEqual('pressure')
-    expect(result.series[2].first.timestamp).toEqual(new Date('2025-03-20T07:30:21.101Z'))
+    expect(result.series[2].first?.timestamp).toEqual(new Date('2025-03-20T07:30:21.101Z'))
   })
 })
--- lib/das-captis-v2/test/das-captis-v2-converter.test.ts
+++ lib/das-captis-v2/test/das-captis-v2-converter.test.ts
@@ -24,37 +24,37 @@ describe('Unit test for Captis converter', function () {
     expect(modbusFlow2Series).toBeDefined()
 
     if (batterySeries) {
-      expect(batterySeries.first.timestamp).toBeDefined()
-      expect(batterySeries.first.value).toEqual(3.58)
+      expect(batterySeries.first?.timestamp).toBeDefined()
+      expect(batterySeries.first?.value).toEqual(3.58)
     }
 
     if (signalStrengthSeries) {
-      expect(signalStrengthSeries.first.timestamp).toBeDefined()
-      expect(signalStrengthSeries.first.value).toEqual(-94)
+      expect(signalStrengthSeries.first?.timestamp).toBeDefined()
+      expect(signalStrengthSeries.first?.value).toEqual(-94)
     }
 
     if (flowSeries) {
-      expect(flowSeries.first.timestamp).toEqual(new Date('2019-08-05T07:02:00Z'))
-      expect(flowSeries.first.value).toEqual(2.0)
+      expect(flowSeries.first?.timestamp).toEqual(new Date('2019-08-05T07:02:00Z'))
+      expect(flowSeries.first?.value).toEqual(2.0)
       expect(flowSeries.values).toEqual([2.0, 4.0, 3145728.0, 3145729.0])
-      expect(flowSeries.last.timestamp).toEqual(new Date('2019-09-17T04:51:01Z'))
+      expect(flowSeries.last?.timestamp).toEqual(new Date('2019-09-17T04:51:01Z'))
     }
 
     if (eventSeries) {
-      expect(eventSeries.first.timestamp).toEqual(new Date('2019-08-06T05:00:53Z'))
-      expect(eventSeries.first.value).toEqual('a00077-00008')
-      expect(eventSeries.last.value).toEqual('a00077-00009')
-      expect(eventSeries.last.timestamp).toEqual(new Date('2019-08-06T05:01:28Z'))
+      expect(eventSeries.first?.timestamp).toEqual(new Date('2019-08-06T05:00:53Z'))
+      expect(eventSeries.first?.value).toEqual('a00077-00008')
+      expect(eventSeries.last?.value).toEqual('a00077-00009')
+      expect(eventSeries.last?.timestamp).toEqual(new Date('2019-08-06T05:01:28Z'))
     }
 
     if (modbusFlow1Series) {
-      expect(modbusFlow1Series.first.timestamp).toEqual(new Date('2019-09-17T05:51:01Z'))
-      expect(modbusFlow1Series.first.value).toEqual(3145730.0)
+      expect(modbusFlow1Series.first?.timestamp).toEqual(new Date('2019-09-17T05:51:01Z'))
+      expect(modbusFlow1Series.first?.value).toEqual(3145730.0)
     }
 
     if (modbusFlow2Series) {
-      expect(modbusFlow2Series.first.timestamp).toEqual(new Date('2019-09-17T06:51:01Z'))
-      expect(modbusFlow2Series.first.value).toEqual(3145731.0)
+      expect(modbusFlow2Series.first?.timestamp).toEqual(new Date('2019-09-17T06:51:01Z'))
+      expect(modbusFlow2Series.first?.value).toEqual(3145731.0)
     }
   })
 })
